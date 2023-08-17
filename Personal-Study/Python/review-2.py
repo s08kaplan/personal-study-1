a792ce3687331a95c65d963989cabfc77432bd99
Serkan <s8ersis@gmail.com>
2023-08-17 07:21:37 +0300

    some fixes

---
+++ Personal-Study/Python/review-2.py
@@ -0,0 +1,24 @@
+#Given the code below, use the correct method in order to find out the number of elements in my_tup.
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+number = len(my_tup)
+print (number)
+
+
+#Given the code below, use the correct method in order to find out the index of Slovakia in my_tup.
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+index = my_tup.index("Slovakia")
+print (index)
+
+
+#Given the code below, write code in order to perform tuple assignment on line 3 and obtain the results below.
+my_tup = ("Romania", "Poland", "Estonia")
+
+
+
+print (ro + ", " + po + ", + es) #returns 'Romania, Poland, Estonia'
+#Given the code below, use the correct method on line 3 in order to find out the last element of my_tup in alphabetical order.
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+last =
