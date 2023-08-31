539bed9d4271a747d1e1fa228e67e40c20fe3ab8
Serkan <s8ersis@gmail.com>
2023-08-31 07:00:52 +0300

    oop-python

---
+++ Personal-Study/Python/from-internet/oop-learning.py
@@ -0,0 +1,25 @@
+class Workers():
+    def __init__(self,name,fname,salary):
+        self.name = name
+        self.fname = fname
+        self.salary = salary
+        self.email = name + fname + "@company.com"
+
+    def show_info(self):
+        return f"Name: {self.name} Surname: {self.fname} Salary: {self.salary} Email: {self.email} "    
+
+worker1 =  Workers("Ali","Guzel",5000)
+worker2 = Workers("Hasan","Mutlu",8000) 
+
+class Programmer(Workers):
+    pass
+
+
+programmer1 = Programmer("Jenny","Leila",9000)
+programmer2= Programmer("Fatma","Yildiz",9000)
+
+print(programmer1.name)
+print(programmer2.fname)
+print(worker1.email)
+print(programmer2.show_info())
+
