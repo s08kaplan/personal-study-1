d7a453e0e6e763914bc762a995498572e48e80af
Serkan <s8ersis@gmail.com>
2023-09-01 22:41:16 +0300

    trying to learn

--- Personal-Study/Python/os-module.py
+++ Personal-Study/Python/os-module.py
@@ -1,7 +1,7 @@
 import os
 print(os.getcwd()) # shows us which folder are we working on
 # os.chdir()  # to change directory
-#print(os.listdir()) shows us what we in folder
+#print(os.listdir()) shows us what we have in folder
 print(os.listdir()) # if you give a parameter inside it you can see the containers in that folder
 #to read the folder's containers we can do like this
 for folder in os.listdir():
@@ -16,3 +16,8 @@ for folder in os.listdir():
  # os.walk() shows us like a tree tho folders subfolders till the last one in another
  # os.path.isfile() and os.path.isdir() if you give an argument it returns true or false 
  # os.path.splitext() returns a tuple first element shows us the path the second one shows us the extension(.exe,.doc,.py etc)
+
+
+print(os.getcwd())
+os.chdir("\Users\HP\Desktop\personal study\Personal-Study\Python\from-internet")
+#print(os.getcwd())
